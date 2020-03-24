c11cc382dd7fbd0118bd89f267a5883b7f8b099e
VFerrari <vferrari@mpc.com.br>
2020-03-24 11:51:41 -0300

    Finishing parser object configuration and main UI

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ University of Campinas - UNICAMP - 2020
 '''
 
 from lexer import uCLexer as Lexer
-from parser import *
+from parser import uCParser as Parser
 from os.path import exists
 
 def print_error(msg, x, y):
     print("Lexical error: %s at %d:%d" % (msg, x, y))
 
 if __name__ == '__main__':
-
+    
+    # Building lexer
+    tokenizer = Lexer(print_error)
+    tokenizer.build()
+        
+    # Building parser
+    parser = Parser(tokenizer)
+    parser.build()
+    
     while True:
         print("\nSend 'l' for lexer test, and 'p' for parser test ('q' to quit)")
         mode = input("Mode: ")
@@ -26,9 +34,6 @@ if __name__ == '__main__':
         if mode == 'l':
             while True: 
                 try:
-                    tokenizer = Lexer(print_error)
-                    tokenizer.build()
-                    
                     tokenizer.test(input("Filename or expression: "))
                     tokenizer.reset_line_num()
                 except EOFError:
@@ -36,12 +41,7 @@ if __name__ == '__main__':
         elif mode == 'p':
             while True:
                 try:
-                    print('Coming soon!')
-                    raise EOFError
-                    #parser = uCParser()
-                    #parser = yacc(write_tables=False)
-                    
-                    #parser.parse(input("Filename or expression for Parser: "))
+                    parser.parse(input("Filename or expression for Parser: "))
                 except EOFError:
                     break
         elif mode == 'q':
--- parser.py
+++ parser.py
@@ -15,7 +15,21 @@ Last Modified: 24/03/2020.
 from ply.yacc import yacc
 
 class uCParser():
-    def p_statement_list(p):
+    
+    # Initializes the class with the lexer object and tokens list.
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.tokens = lexer.tokens
+    
+    # Builds the parser.
+    def build(self, **kwargs):
+        self.parser = yacc(module=self, write_tables=False)
+    
+    # Parses an expression.
+    def parse(self, data):
+        self.parser.parse(data)
+    
+    def p_statement_list(self, p):
         ''' statements : statements statement
                        | statement
         '''
@@ -24,39 +38,40 @@ class uCParser():
         else:
             p[0] = p[1] + (p[2]) 
 
-    def p_assign_statement (p):
+    def p_assign_statement (self, p):
         ''' statement : ID '=' expr
         '''
         p[0] = ('assign', p[1], p[3])
         
-    def p_print_statement (p):
+    def p_print_statement (self, p):
         ''' statement : PRINT '(' expr ')'
         '''
         p[0] = ('print', p[3])
+        print('print')
         
-    def p_binop_expr (p):
+    def p_binop_expr (self, p):
         ''' expr : expr '+' expr
                  | expr '*' expr
         '''
         p[0] = (p[2], p[1], p[3])
         
-    def p_num_expr (p):
+    def p_num_expr (self, p):
         ''' expr : ICONST
                  | FCONST
         '''
         p[0] = ('num', p[1])
         
-    def p_name_expr (p):
+    def p_name_expr (self, p):
         ''' expr : ID
         '''
         p[0] = ('id', p[1])
         
-    def p_compound_expr (p):
+    def p_compound_expr (self, p):
         ''' expr : '(' expr ')'
         '''
         p[0] = p[2]
     
-    def p_error (p):
+    def p_error (self, p):
         if p:
             print("Error near the symbol %s" % p.value)
         else:
