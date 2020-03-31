6aee18f47d8d629bfa652f09a805ae035f56f3d0
VFerrari <vferrari@mpc.com.br>
2020-03-31 10:29:21 -0300

    Adding empty statement node, and NodeVisitor

--- uCAST.py
+++ uCAST.py
@@ -106,6 +106,64 @@ class Node(object):
     # NOTE: it seems to be a list for variables contained in the class (excluding subtrees)
     attr_names = () 
 
+class NodeVisitor(object):
+    """ A base NodeVisitor class for visiting uc_ast nodes.
+        Subclass it and define your own visit_XXX methods, where
+        XXX is the class name you want to visit with these
+        methods.
+
+        For example:
+
+        class ConstantVisitor(NodeVisitor):
+            def __init__(self):
+                self.values = []
+
+            def visit_Constant(self, node):
+                self.values.append(node.value)
+
+        Creates a list of values of all the constant nodes
+        encountered below the given node. To use it:
+
+        cv = ConstantVisitor()
+        cv.visit(node)
+
+        Notes:
+
+        *   generic_visit() will be called for AST nodes for which
+            no visit_XXX method was defined.
+        *   The children of nodes for which a visit_XXX was
+            defined will not be visited - if you need this, call
+            generic_visit() on the node.
+            You can use:
+                NodeVisitor.generic_visit(self, node)
+        *   Modeled after Python's own AST visiting facilities
+            (the ast module of Python 3.0)
+    """
+
+    _method_cache = None
+
+    def visit(self, node):
+        """ Visit a node.
+        """
+
+        if self._method_cache is None:
+            self._method_cache = {}
+
+        visitor = self._method_cache.get(node.__class__.__name__, None)
+        if visitor is None:
+            method = 'visit_' + node.__class__.__name__
+            visitor = getattr(self, method, self.generic_visit)
+            self._method_cache[node.__class__.__name__] = visitor
+
+        return visitor(node)
+
+    def generic_visit(self, node):
+        """ Called if no explicit visitor function exists for a
+            node. Implements preorder visiting of the node.
+        """
+        for c in node:
+            self.visit(c)
+
 # This is the top of the AST, representing a uC program (a
 # translation unit in K&R jargon). It contains a list of
 # global-declaration's, which is either declarations (Decl),
@@ -187,6 +245,9 @@ class Break(Node):
     def __init__(self, coord=None):
         self.coord = coord
     
+    def children(self):
+        return ()
+        
     attr_names = ()
 
 class Cast(Node):
@@ -239,7 +300,15 @@ class DeclList(Node):
     __slots__ = ()
 
 class EmptyStatement(Node):
-    __slots__ = ()
+    __slots__ = ('coord')
+    
+    def __init__(self, coord=None):
+        self.coord = coord
+        
+    def children(self):
+        return ()
+        
+    attr_names = ()
 
 class ExprList(Node):
     __slots__ = ()
